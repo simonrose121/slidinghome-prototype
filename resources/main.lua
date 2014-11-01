5b34fe035aff5783d10f176b3475017ab680e112
Simon Rose <simonrose121@hotmail.co.uk>
2014-11-01 17:16:27 +0000

    movement left, right up and down on swipe (slightly buggy)

--- resources/main.lua
+++ resources/main.lua
@@ -2,45 +2,26 @@ local gem = director:createSprite({
     x=director.displayWidth / 2,
     y=director.displayHeight / 2,
     source="textures/gem.png",
-    xAnchor=0.5,
-    yAnchor=0.5
 })
 
 local gemTween
-local currentScore = 0
-
--- Create a label to display the score
-local scoreLabel = director:createLabel( {
-    x=0, y=director.displayHeight - 30,
-    w=director.displayWidth, h = 30,
-    hAlignment="left", vAlignment="top",
-    text="0",
-    color=color.yellow
-    })
   
--- Create an event handler function that is called when the user touches the gem
-function gemTouched(event)
-    if (event.phase == "ended") then
-        -- Update the score
-        currentScore = currentScore + 10
-        scoreLabel.text = currentScore
-        if (gemTween == nil) then
-            -- Create a tween that scales the gem up and down
-            gemTween = tween:to(gem, {
-                xScale=2,
-                time=1,
-                mode="mirror"
-                } )
-        else
-            -- Cancel the tween
-            tween:cancel(gemTween)
-            -- Destroy the tween
-            gemTween = nil
-            --Reset the gems xScale to 1.0
-            gem.xScale = 1
+function screenTouched(event)
+    if (event.phase == "moved") then
+        --swipe up to move gem down
+        if (event.y > gem.y) then
+            gemTween = tween:to(gem, { y=0, time=1, easing=ease.powIn, easingValue=3 })
+        --swipe right to move left
+        elseif (event.x < gem.x) then
+            gemTween = tween:to(gem, { x=0, time=1, easing=ease.powIn, easingValue=3 })
+        --swipe down to move gem up
+        elseif (event.y < gem.y) then
+            gemTween = tween:to(gem, { y=director.displayHeight-25, time=1, easing=ease.powIn, easingValue=3 })
+        --swipe left to move gem right
+        elseif (event.x > gem.x) then
+            gemTween = tween:to(gem, { x=director.displayWidth-25, time=1, easing=ease.powIn, easingValue=3 })
         end
     end
 end
   
--- Add the gemTouched event handler function to the sprites "touch" event listener
-gem:addEventListener("touch", gemTouched)
+system:addEventListener("touch", screenTouched)
