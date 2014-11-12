064c433fcd4eea91b9f854f41e056734c5739d2c
Simon Rose <simonrose121@hotmail.co.uk>
2014-11-12 13:14:45 +0000

    collision detection and a grid

--- resources/main.lua
+++ resources/main.lua
@@ -7,38 +7,59 @@ local gemTween
 local startX
 local startY
 
+local map = {
+      { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+      { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+      { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+      { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+      { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+      { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+      { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+      { 1, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 1 },
+      { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+      { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+      { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+      { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }
+  }
+
 local debug = director:createLabel( {
     x=0, y=director.displayHeight - 30,
     w=director.displayWidth, h = 30,
     hAlignment="left", vAlignment="top",
-    text="0",
     color=color.yellow
 })
 
 function createGrid()
-  grid = {}
-  for px = 1, 11 do
-    grid[px] = {}
-    for py = 0, 18 do
-      grid[px][py] = director:createSprite({
-        source="textures/gridbox.png",
-        x=px*cellsize,
-        y=py*cellsize,
-        xScale=scale,
-        yScale=scale
-      })
-    end
+  for x = 1, #map do
+      for y = 1, #map[x] do
+          if map[x][y] == 1 then
+              director:createSprite({
+                  source="textures/gridbox.png",
+                  x=y*cellsize,
+                  y=x*cellsize,
+              })
+          end
+          if map[x][y] == 2 then
+              gem = director:createSprite({
+                  source="textures/gem.png",
+                  x=y*cellsize,
+                  y=x*cellsize,
+                  xScale=scale,
+                  yScale=scale
+              })
+          end
+      end
   end
 end
 
-function initGem() 
-  gem = director:createSprite({
-    source="textures/gem.png",
-    x=cellsize*6,
-    y=cellsize*9,
-    xScale=scale,
-    yScale=scale
-  })
+function testMap(x ,y)
+    local tempx = gem.y / cellsize + y
+    local tempy = gem.x / cellsize + x
+    debug.text = "x " .. tempx .. " y " .. tempy .. " map " .. map[tempx][tempy]
+    if map[tempx][tempy] == 1 then
+        return false
+    end
+    return true
 end
 
 function touch(event)  
@@ -47,23 +68,26 @@ function touch(event)
       startY = event.y
     end
     if (event.phase == "moved") then
-      debug.text = "startX = " .. startX .. "\nevent.x = " .. event.x .. "\nstartY = " .. startY .. "\nevent.y = " .. event.y 
       if (gemTween == nil) then
-        --left
         if (event.x < startX and event.y < startY+swipeOffset and event.y > startY-swipeOffset) then
-          gemTween = tween:to(gem, { x=gem.x-cellsize, time=0.02 })
-        --end
+            if testMap(-1, 0) then
+                gemTween = tween:to(gem, { x=gem.x-cellsize, time=0.02 })
+            end
         --up
         elseif (event.y < startY and event.x < startX+swipeOffset and event.x > startX-swipeOffset) then
-          gemTween = tween:to(gem, { y=gem.y-cellsize, time=0.02 })
-        --end
+            if testMap(0, -1) then
+                gemTween = tween:to(gem, { y=gem.y-cellsize, time=0.02 })
+            end
         --right
         elseif (event.x > startX and event.y < startY+swipeOffset and event.y > startY-swipeOffset) then
-          gemTween = tween:to(gem, { x=gem.x+cellsize, time=0.02 })
-        --end
+            if testMap(1, 0) then
+                gemTween = tween:to(gem, { x=gem.x+cellsize, time=0.02 })
+            end
         --down
         elseif (event.y > startY and event.x < startX+swipeOffset and event.x > startX-swipeOffset) then
-          gemTween = tween:to(gem, { y=gem.y+cellsize, time=0.02 })
+            if testMap(0, 1) then
+                gemTween = tween:to(gem, { y=gem.y+cellsize, time=0.02 })
+            end
         end
       end
     end
@@ -73,6 +97,10 @@ function touch(event)
     end
 end
 
+function cancelGemTween()
+    tween:cancel(gemTween)
+    gemTween = nil
+end
+
 createGrid()
-initGem()
 system:addEventListener("touch", touch)
