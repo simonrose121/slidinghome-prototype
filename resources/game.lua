8f7accb740bbc3b9422d3abab50f48428c1bc926
Simon Rose <simonrose121@hotmail.co.uk>
2014-11-13 11:59:13 +0000

    Can slide over previous position, gradual slide + transitions

--- resources/game.lua
+++ resources/game.lua
@@ -6,12 +6,13 @@ local gem
 local cellsize = 64
 local scale = 1
 local swipeOffset = 100
-local speed = 1
+local speed = 8
 local gemTween
 local startX
 local startY
 local tmpX
 local tmpY
+local index = 1
 
 local map = {
       { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
@@ -70,22 +71,19 @@ function testObstacle(xDir, yDir)
    -- Contract: 
    --           either xDir or yDir must be 0
    --           xDir and yDir can only be -1,0,1
-    local index = 1
+    index = 1
     local playerX = gem.x / cellsize
     local playerY = gem.y / cellsize
-    --print("Player postion: " .. playerX .. ", " .. playerY)
-    --print("Testing postion: " .. (playerX + (xDir*index)) .. ", " .. (playerY + (yDir*index)))
     while(map[playerX + (xDir*index)][playerY + (yDir*index)] == 0) do
-      --print("Position is free!")
       index = (index + 1)
-      --print("Testing postion: " .. (playerX + (xDir*index)) .. ", " .. (playerY + (yDir*index)))
+      map[playerX][playerY] = 0
     end
     tmpX = (playerX + (xDir * (index-1)))
     tmpY = (playerY + (yDir * (index-1)))
-    --print("Player's potential postion: " .. tmpX .. ", " .. tmpY)
 end
 
 function testMap(xDir, yDir)
+  print("hit")
     local playerX = gem.x / cellsize
     local playerY = gem.y / cellsize
     if map[playerX + xDir][playerY + yDir] == 3 then
@@ -102,20 +100,28 @@ function touch(event)
         if (gemTween == nil) then
             if (event.x < startX and event.y < startY+swipeOffset and event.y > startY-swipeOffset) then
                 testObstacle(-1, 0)
-                gemTween = tween:to(gem, { x=tmpX*cellsize, time=speed, onComplete=cancelTween })
+                gemTween = tween:to(gem, { x=tmpX*cellsize, time=index/speed, onComplete=cancelTween, easing=ease.sineIn })
             --up
             elseif (event.y < startY and event.x < startX+swipeOffset and event.x > startX-swipeOffset) then
                 testObstacle(0, -1)
-                gemTween = tween:to(gem, { y=tmpY*cellsize, time=speed, onComplete=cancelTween })
+                gemTween = tween:to(gem, { y=tmpY*cellsize, time=index/speed, onComplete=cancelTween, easing=ease.sineIn })
             --right
             elseif (event.x > startX and event.y < startY+swipeOffset and event.y > startY-swipeOffset) then
                 testObstacle(1, 0)
-                gemTween = tween:to(gem, { x=tmpX*cellsize, time=speed, onComplete=cancelTween })
+                gemTween = tween:to(gem, { x=tmpX*cellsize, time=index/speed, onComplete=cancelTween, easing=ease.sineIn })
             --down
             elseif (event.y > startY and event.x < startX+swipeOffset and event.x > startX-swipeOffset) then
                 testObstacle(0, 1)
-                gemTween = tween:to(gem, { y=tmpY*cellsize, time=speed, onComplete=cancelTween })
+                gemTween = tween:to(gem, { y=tmpY*cellsize, time=index/speed, onComplete=cancelTween, easing=ease.sineIn })
             end
+        end
+    end
+end
+
+function key(event)
+    if(event.phase == "pressed")
+        if(event.key == "up") then
+            print("up")
         end
     end
 end
@@ -131,6 +137,7 @@ end
 
 createGrid()
 system:addEventListener("touch", touch)
+system:addEventListener("pressed", key)
 
 function pauseGame(event)
 	if (event.phase == "ended") then
@@ -141,10 +148,10 @@ end
 
 -- Create pause menu sprite (docked to top of screen)
 local pause_sprite = director:createSprite( {
-	x = director.displayCenterX, y = 0, 
-	xAnchor = 0.5, 
-	source = "textures/pause_icon.png"
-	} )
+  	x = director.displayCenterX, y = 0, 
+  	xAnchor = 0.5, 
+  	source = "textures/pause_icon.png"
+} )
 sprite_w, sprite_h = pause_sprite:getAtlas():getTextureSize()
 pause_sprite.y = director.displayHeight - sprite_h
 pause_sprite:addEventListener("touch", pauseGame)
--- resources/main.lua
+++ resources/main.lua
@@ -6,12 +6,12 @@ require("game")
 -- Switch to specific scene
 function switchToScene(scene_name)
     if (scene_name == "game") then
-        director:moveToScene(gameScene, {transitionType="slideInL", transitionTime=0.5})
+        director:moveToScene(gameScene, {transitionType="slideInB", transitionTime=0.5})
     elseif (scene_name == "main") then
-        director:moveToScene(menuScene, {transitionType="slideInL", transitionTime=0.5})
+        director:moveToScene(menuScene, {transitionType="fade", transitionTime=0.5})
     elseif (scene_name == "pause") then
-        director:moveToScene(pauseScene, {transitionType="slideInL", transitionTime=0.5})
+        director:moveToScene(pauseScene, {transitionType="slideInT", transitionTime=0.5})
     elseif (scene_name == "end") then
-        director:moveToScene(endScene, {transitionType="slideInL", transitionTime=0.5})    
+        director:moveToScene(endScene, {transitionType="fade", transitionTime=0.5})    
     end
 end
